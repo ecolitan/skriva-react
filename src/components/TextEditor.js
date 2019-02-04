ed3b746199c2769af987ce981904f025ab02a9ea
Aaron Cossey <aaron@formbay.com.au>
2019-02-04 14:58:02 +0100

    Add more formatting options

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -5,6 +5,9 @@ import { Value } from 'slate'
 import Icon from 'react-icons-kit'
 import { bold } from 'react-icons-kit/feather/bold'
 import { italic } from 'react-icons-kit/feather/italic'
+import { code } from 'react-icons-kit/feather/code'
+import { list } from 'react-icons-kit/feather/list'
+import { underline } from 'react-icons-kit/feather/underline'
 
 import { BoldMark, ItalicMark } from './index';
 import FormatToolbar from "./FormatToolbar";
@@ -55,12 +58,27 @@ export default class TextEditor extends Component {
             /* When "b" is pressed, add a bold mark to the text. */
             case 'b': {
                 change.toggleMark('bold');
-                return true
+                return true;
             }
 
             case 'i': {
                 change.toggleMark('italic');
-                return true
+                return true;
+            }
+
+            case 'c': {
+                change.toggleMark('code');
+                return true;
+            }
+
+            case 'l': {
+                change.toggleMark('list');
+                return true;
+            }
+
+            case 'u': {
+                change.toggleMark('underline');
+                return true;
             }
 
             default: {
@@ -80,6 +98,19 @@ export default class TextEditor extends Component {
 
             case 'italic':
                 return <ItalicMark {...props} />;
+
+            case 'code':
+                return <code {...props.attributes}>{props.children}</code>;
+
+            case 'list':
+                return (
+                    <ul {...props.attributes}>
+                        <li>{props.children}</li>
+                    </ul>
+                );
+
+            case 'underline':
+                return <u {...props.attributes}>{props.children}</u>;
 
             default: {
                 return;
@@ -109,6 +140,24 @@ export default class TextEditor extends Component {
                         className='tooltip-icon-button'
                     >
                         <Icon icon={italic} />
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'code')}
+                        className='tooltip-icon-button'
+                    >
+                        <Icon icon={code}/>
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'list')}
+                        className='tooltip-icon-button'
+                    >
+                        <Icon icon={list}/>
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'underline')}
+                        className='tooltip-icon-button'
+                    >
+                        <Icon icon={underline}/>
                     </button>
                 </FormatToolbar>
                 <Editor
